8f6d63656eb2e930eb3260eaa26e55eda4a5aefe
linkkoll <89920100+linkkoll@users.noreply.github.com>
2023-10-26 21:45:22 -0300

    Create data.php

---
+++ aula4/data.php
@@ -0,0 +1,30 @@
+<?php
+
+//Lista de dados: array associativo
+$movies = array(
+    0 => [
+        "filme" => "Os batutinhas",
+        "ano" => 1994,
+        "genero" => "Comédia"
+    ],
+    1 => [
+        "filme" => "Peter pan",
+        "ano" => 2003,
+        "genero" => "Aventura"
+    ],
+    2 => [
+        "filme" => "Frozen",
+        "ano" => 2016,
+        "genero" => "Aventura"
+    ],
+    3 => [
+        "filme" => "Wall-e",
+        "ano" => 2008,
+        "genero" => "Ficção"
+    ],
+    4 => [
+        "filme" => "Náufrago",
+        "ano" => 2000,
+        "genero" => "Drama"
+    ]
+);
