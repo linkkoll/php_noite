1795ec77153a258f56069317c678b1abbab43ab4
linkkoll <89920100+linkkoll@users.noreply.github.com>
2023-11-16 21:50:11 -0300

    Create database.php

---
+++ aula10_noite/database.php
@@ -0,0 +1,14 @@
+<?php
+
+//Criando lista de dados
+$database = array(
+    0 => [
+        "user" => "admin",
+        "pass" => "1234",
+        "photo" => "/aula10_noite/assets/img/profile/perfilAdm.png"
+    ],
+    1 => [
+        "user" => "ja1",
+        "pass" => "1234"
+    ],
+);
